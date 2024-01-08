d8d05a51a3f8fd325bc449956ae339693c802a56
20dlwnsrhkd <jimmy010011@gmail.com>
2024-01-08 15:16:13 +0900

    day1

--- DAY1_Program1.cs
+++ DAY1_Program1.cs
@@ -1,18 +1,21 @@
-namespace Hello_world1; // 클래스 접근, 중첩 x
+namespace Hello_world1{ // 네임스페이스 괄호 적용
+public class DAY1_Program1 // 접근 지정자 공개로 설정해야 함
+{
+public DAY1_Program1(){ // 접근 지정자 공개
+String man ="아저씨2";
+System.Console.WriteLine("Hello, World2!"); // system 직접 추가
+Console.WriteLine(man);
+}
+}
+}
+namespace Hello_world2{ // 다른 네임 스페이스
 class DAY1_Program1
 {
-DAY1_Program1(){ // 생성자 정의, 프로그램 실행 시 자동 수행
+static void Main1(string[] args){ // 실행 기준
 String man ="아저씨1";
 Console.WriteLine("Hello, World1!");
 Console.WriteLine(man);
+Hello_world1.DAY1_Program1 test = new Hello_world1.DAY1_Program1(); // 네임스페이스로 호출
 }
-}
-
-class DAY1_Program_
-{
-static void Main(String[] args){ // 생성자 추가 가능
-String man ="아저씨2";
-Console.WriteLine("Hello, World2!");
-Console.WriteLine(man);
 }
 }
