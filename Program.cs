dd5cfed8f668e81bdf97dcaf7f6be4d5b0fee756
20dlwnsrhkd <jimmy010011@gmail.com>
2024-01-08 11:40:36 +0900

    DAY1_PROJECT

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Hello_world; // 네임스페이스 : 고유 식별, 별명
+class Program // 클래스 정의
+{ // 시작
+static void Main(string[] args) // 시작 Main 메서드, 1개 이상 x
+{
+Console.WriteLine("Hello, World!"); // 콘솔 출력
+}
+} // 끝
+
