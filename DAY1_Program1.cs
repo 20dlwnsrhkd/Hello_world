7b60f40df3a4598b80959f93781879d70f831c35
20dlwnsrhkd <jimmy010011@gmail.com>
2024-01-08 14:25:49 +0900

    DAY 1

---
+++ DAY1_Program1.cs
@@ -0,0 +1,18 @@
+namespace Hello_world1; // 클래스 접근, 중첩 x
+class DAY1_Program1
+{
+DAY1_Program1(){ // 생성자 정의, 프로그램 실행 시 자동 수행
+String man ="아저씨1";
+Console.WriteLine("Hello, World1!");
+Console.WriteLine(man);
+}
+}
+
+class DAY1_Program_
+{
+static void Main(String[] args){ // 생성자 추가 가능
+String man ="아저씨2";
+Console.WriteLine("Hello, World2!");
+Console.WriteLine(man);
+}
+}
